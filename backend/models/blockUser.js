919e5df00ca889cfa691697c651caed0a1cac070
Jatin Lalit <freemindedjatin@gmail.com>
2023-05-08 20:27:47 +0530

    Delete backend directory

--- backend/models/blockUser.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const blockSchema = mongoose.Schema({
-  token: { type: String, required: true },
-});
-
-const BlockModel = mongoose.model("blacklist", blockSchema);
-module.exports = { BlockModel };
